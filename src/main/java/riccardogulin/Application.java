f0b659d5823aabccd0708874bf6efb6d83e1ce6a
Riccardo Gulin <riccardo.gulin@gmail.com>
2024-10-03 12:29:14 +0200

    Adds MapToInt examples

--- src/main/java/riccardogulin/Application.java
+++ src/main/java/riccardogulin/Application.java
@@ -54,5 +54,58 @@ public class Application {
 		// 8. Raggruppiamo per città e calcoliamo varie statistiche come media età, somma età, età minima, età massima, ecc
 		Map<String, IntSummaryStatistics> statsPerCity = randomUsers.stream().collect(Collectors.groupingBy(user -> user.getCity(), Collectors.summarizingInt(user -> user.getAge())));
 		statsPerCity.forEach((city, stats) -> System.out.println("Città: " + city + ", " + stats));
+
+		// ****************************************** COMPARATORS ***************************************
+		System.out.println("****************************************** COMPARATORS ***************************************");
+		// 1. Ordiniamo la lista utenti per età (Ordine crescente)
+		List<User> usersSortedByAge = randomUsers.stream().sorted(Comparator.comparingInt(user -> user.getAge())).toList();
+		usersSortedByAge.forEach(user -> System.out.println(user));
+
+		// 2. Ordiniamo la lista utenti per età (Ordine decrescente)
+		System.out.println("Ordiniamo la lista utenti per età (Ordine decrescente)");
+		List<User> usersSortedByAgeDesc = randomUsers.stream().sorted(Comparator.comparingInt(User::getAge).reversed()).toList();
+		// Se si utilizza la reversed non ci è permesso di utilizzare la lambda ma devo utilizzare la forma compatta User::getAge
+		usersSortedByAgeDesc.forEach(user -> System.out.println(user));
+
+		// 3. Ordiniamo la lista utenti per cognome (Ordine crescente)
+		System.out.println("Ordiniamo la lista utenti per cognome (Ordine crescente)");
+		List<User> usersSortedBySurname = randomUsers.stream().sorted(Comparator.comparing(User::getSurname)).toList();
+		usersSortedBySurname.forEach(user -> System.out.println(user));
+
+		// ****************************************** LIMIT ***************************************
+		System.out.println("****************************************** LIMIT ***************************************");
+		// 1. Otteniamo i 5 user più vecchi, tramite il sorted li ordino per età decrescente, poi tramite limit ne tengo solo i primi 5.
+		// Opzionalmente posso usare skip() per anche decidere di saltare tot elementi per ottenere ad esempio gli elementi dal 96esimo al 100esimo
+		List<User> fiveOldUsers = randomUsers.stream().sorted(Comparator.comparingInt(User::getAge).reversed()).skip(95).limit(5).toList();
+		System.out.println(fiveOldUsers);
+
+		// ****************************************** MAP TO ***************************************
+		// 1. Calcolo della somma delle età tramite reduce
+		System.out.println("1. Calcolo della somma delle età tramite reduce");
+		int totalAge = randomUsers.stream().map(User::getAge).reduce(0, (acc, age) -> acc + age);
+		System.out.println("Somma delle età tramite reduce: " + totalAge);
+
+		// 2. Calcolo della somma delle età tramite mapToInt
+		System.out.println("2. Calcolo della somma delle età tramite mapToInt");
+		int totalAge2 = randomUsers.stream().mapToInt(user -> user.getAge()).sum();
+		System.out.println("Somma delle età tramite mapToInt: " + totalAge2);
+
+		// 3. Calcolo della media delle età tramite mapToInt
+		System.out.println("3. Calcolo della media delle età tramite mapToInt");
+		OptionalDouble average2 = randomUsers.stream().mapToInt(user -> user.getAge()).average();
+		if (average2.isPresent()) System.out.println("La media è: " + average2.getAsDouble());
+			// Se la media c'è (la lista ha più di zero elementi) ci facciamo tornare un double
+		else System.out.println("Non è possibile calcolare la media perché la lista è vuota");
+
+		// 4. Calcolo dell'età maggiore tramite mapToInt
+		System.out.println("4. Calcolo dell'età maggiore tramite mapToInt");
+		OptionalInt maxAge = randomUsers.stream().mapToInt(user -> user.getAge()).max();
+		if (maxAge.isPresent()) System.out.println("L'età massima è: " + maxAge.getAsInt());
+		else System.out.println("Non è possibile trovare il più vecchio perché la lista è vuota");
+
+		// 5. Ottenimento di statistiche sulle età tramite mapToInt
+		System.out.println("5. Ottenimento di statistiche sulle età tramite mapToInt");
+		IntSummaryStatistics statistics = randomUsers.stream().mapToInt(user -> user.getAge()).summaryStatistics();
+		System.out.println(statistics);
 	}
 }
